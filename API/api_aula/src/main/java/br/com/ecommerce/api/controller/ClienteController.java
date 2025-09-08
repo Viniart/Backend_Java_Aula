0152b3c10484cb6e55a08458448dae0e6ede83c0
Vinicio <seu@email.com>
2025-09-08 14:12:40 -0300

    :tada: Subindo projeto - Aula 05/09

---
+++ API/api_aula/src/main/java/br/com/ecommerce/api/controller/ClienteController.java
@@ -0,0 +1,30 @@
+package br.com.ecommerce.api.controller;
+
+import br.com.ecommerce.api.model.Cliente;
+import br.com.ecommerce.api.service.ClienteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+    // Controller -> Service
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService service) {
+        clienteService = service;
+    }
+
+    // Listar Todos
+    @GetMapping
+    public ResponseEntity<List<Cliente>> listarClientes() {
+        // 1. Pegar a lista de clientes
+        List<Cliente> clientes = clienteService.listarTodos();
+
+        return ResponseEntity.ok(clientes);
+    }
+}
