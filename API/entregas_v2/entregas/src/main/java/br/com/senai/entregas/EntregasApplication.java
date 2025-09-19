446b81d15c0a02bd301a94f8f397ccef455f8864
Vinicio <seu@email.com>
2025-09-19 15:17:00 -0300

    :sparkles: feat: Adicionado UserDetails e método para buscar por email

--- API/entregas_v2/entregas/src/main/java/br/com/senai/entregas/EntregasApplication.java
+++ API/entregas_v2/entregas/src/main/java/br/com/senai/entregas/EntregasApplication.java
@@ -1,9 +1,16 @@
 package br.com.senai.entregas;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "API do Projeto UrbanSwift",
+        version = "1.0",
+        description = "API responsável pelo gerenciamento de usuários, entregas e veiculos da empresa UrbanSwift"
+))
 public class EntregasApplication {
 
 	public static void main(String[] args) {
